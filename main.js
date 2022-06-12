2bbafb867f8e5ee6f4ad2f6b4855b8414e95799c
this-is-yello <yello_3617@naver.com>
2022-06-13 02:28:00 +0900

    220613

--- main.js
+++ main.js
@@ -117,11 +117,14 @@ function taekwondoTech() {
             if (!techSlide.children[j].classList.contains("in-out-left")) {
               techSlide.children[j].classList.add("in-out-right");
               menuSlide.children[j].classList.add("in-out-right");
+
               setTimeout(() => {
                 if (techSlide.children[j].classList.contains("in-out-right")) {
                   techSlide.children[j].classList.remove("in-out-right");
                   techSlide.children[j].classList.add("in-out-left");
+                  techBtns.children[j].classList.add("main-color");
                 }
+
                 if (menuSlide.children[j].classList.contains("in-out-right")) {
                   menuSlide.children[j].classList.remove("in-out-right");
                   menuSlide.children[j].classList.add("in-out-left");
@@ -129,21 +132,23 @@ function taekwondoTech() {
               }, 500);
             }
           }
+
           setTimeout(() => {
             moveCheck = true;
           }, 500);
+
           techSlide.children[i].classList.remove("in-out-left");
           menuSlide.children[i].classList.remove("in-out-left");
         }
       }
-
+      
       // if (menuSlide.children[i].classList.contains("in-out-left")) {
-      //   if (moveCheck) {
-      //     moveCheck = false;
+        //   if (moveCheck) {
+          //     moveCheck = false;
       //     for (let k = 0; k < techBtns.childElementCount; k++) {
-      //       if (!menuSlide.children[k].classList.contains("in-out-left")) {
-      //         menuSlide.children[k].classList.add("in-out-right");
-      //         setTimeout(() => {
+        //       if (!menuSlide.children[k].classList.contains("in-out-left")) {
+          //         menuSlide.children[k].classList.add("in-out-right");
+          //         setTimeout(() => {
       //           if (menuSlide.children[k].classList.contains("in-out-right")) {
       //             menuSlide.children[k].classList.remove("in-out-right");
       //             menuSlide.children[k].classList.add("in-out-left");
@@ -152,11 +157,11 @@ function taekwondoTech() {
       //       }
       //     }
       //     setTimeout(() => {
-      //       moveCheck = true;
-      //     }, 500);
-      //     menuSlide.children[i].classList.remove("in-out-left");
-      //   }
-      // }
+        //       moveCheck = true;
+        //     }, 500);
+        //     menuSlide.children[i].classList.remove("in-out-left");
+        //   }
+        // }
     });
   }
 }
@@ -235,18 +240,11 @@ function goToMain() {
 }
 
 /* ---------- scroll ---------- */
-const locationMain = document.getElementById("main");
-const mainHeight = locationMain.getBoundingClientRect().height;
 
-window.addEventListener("scroll", () =>  {
-  
-})
 // const section = document.querySelectorAll(".section");
 // const sectionHeight = section.getBoundingClientRect().height;
 
-/*  */
-//https://enai.tistory.com/33
-
 /* scroll */
+//https://enai.tistory.com/33
 //https://velog.io/@jaenny/JavaScript-%EC%8A%A4%ED%81%AC%EB%A1%A4-%EC%8B%9C-%ED%95%9C-%EC%84%B9%EC%85%98%EC%94%A9-%EC%9D%B4%EB%8F%99%ED%95%98%EA%B2%8C-%ED%95%98%EA%B8%B0
 //http://2nusa.blogspot.com/2019/04/javascript-mouse-wheel.html
